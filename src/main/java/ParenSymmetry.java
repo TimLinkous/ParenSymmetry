d5a14669214fe746afc8cbdf83ec531f5129a55d
Tim L <timl@Zipcoders-MacBook-Pro.local>
2024-07-06 09:39:58 -0400

    finished

--- src/main/java/ParenSymmetry.java
+++ src/main/java/ParenSymmetry.java
@@ -1,24 +1,35 @@
+import java.io.*;
 
 public class ParenSymmetry {
 
 
-    private Boolean isBalanced(String s) {
-        // implement this method
-        return null;
+    private Boolean isBalanced(String s) {// implement this method
+        int count = 0;
+        for (char c : s.toCharArray()){
+            if (c == '(') count++;// count forward with (, backward with ), should be 0 if balanced
+            if (c == ')') count--;
+        }
+        return count == 0;
     }
 
-    private void checkFile(String filename) {
+    private void checkFile(String filename) throws IOException {
         // open file named filename
-
-        // for each line in the file
-            // read the line
-            // print whether or not the line's parenthesis are balanced
-
-        // CLOSE the file
+        BufferedReader reader = new BufferedReader(new FileReader(filename));
+        String line;//         for each line in the file
+        while ((line = reader.readLine()) != null) {// read the line
+            System.out.println(isBalanced(line));// print whether or not the line's parenthesis are balanced
+        }
+        reader.close();// count forward with (, backward with ), should be 0 if balanced
     }
 
-    public static void main(String[] args) {
+
+
+
+
+
+    public static void main(String[] args) throws IOException {
         ParenSymmetry ps = new ParenSymmetry();
+
 
         Boolean b0 = ps.isBalanced("()");
         printResult(b0, true);
@@ -37,17 +48,21 @@ public class ParenSymmetry {
         }
         printResult(trues, true);
 
+        ps.checkFile("TestStrings0.txt");
+
     }
 
-    private static void printResult(Boolean b0, boolean b) {
-        if (b0 == null) {
-            System.out.println("Null Failure");
-            return;
-        }
-        if (b0 == b) {
-            System.out.println("Success");
-        } else {
-            System.out.println("Failure");
+
+        private static void printResult (Boolean b0,boolean b){
+            if (b0 == null) {
+                System.out.println("Null Failure");
+                return;
+            }
+            if (b0 == b) {
+                System.out.println("Success");
+            } else {
+                System.out.println("Failure");
+            }
         }
     }
-}
+
